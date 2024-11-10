38dfdf674c7b958b61e1f92535db3e94132b0f3a
devashreegoenka <devashree@uchicago.edu>
2024-11-10 16:34:51 +0000

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,17 @@ Rails.application.routes.draw do
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+  post("/insert_item", { :controller => "directors", :action => "create_row" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
 
   get("/movies", { :controller => "movies", :action => "index" })
+  post("/insert_item", { :controller => "movies", :action => "create_row" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   
   get("/actors", { :controller => "actors", :action => "index" })
+  post("/insert_item", { :controller => "actors", :action => "create_row" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+
+
+
 end
